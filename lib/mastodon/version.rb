10d2b559006ab4d6ed63cf88a6439cda75865976
pbzweihander <pbzweihander@gmail.com>
2023-10-23 20:29:47 +0900

    Tag aquarium-yuri-garden

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      'YRYRi'
+      'aquariumyurigarden'
     end
 
     def to_a
@@ -44,7 +44,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'funamitech/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'pbzweihander/mastodon')
     end
 
     def source_base_url
